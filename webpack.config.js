13744b258a2f8ffe3dbb466e81da6cdd95744ad0
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2022-05-03 20:29:27 -0500

    Fix a bug: ADD Env Script

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,6 @@ module.exports = {
         filename: '[name].[contenthash].js',
         assetModuleFilename: 'assets/[hash][ext][query]'
     },
-    mode: 'production',
     resolve: {
         extensions: ['.js'],
         alias: {
